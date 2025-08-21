5491015eeba8aad40e6654b986d80556bd6e2c80
Luke Johnson <lukejohnson32@gmail.com>
2025-08-21 22:07:43 +0100

    styling evening, new nav, dark and light mode styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,54 +1,164 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Button } from './Button';
 
+import {
+	Disclosure,
+	DisclosureButton,
+	DisclosurePanel,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuItems,
+} from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+function classNames(...classes: any) {
+	return classes.filter(Boolean).join(' ');
+}
+
 export default function Navigation() {
+	const location = useLocation();
 	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-	const navLinks =
+	const navigation =
 		!isAuthenticated ?
 			[
-				{ title: 'Home', href: '/' },
-				{ title: 'About', href: '/about' },
+				{ name: 'Home', href: '/', current: true },
+				{ name: 'About', href: '/about', current: false },
 			]
 		:	[
-				{ title: 'Dashboard', href: '/dashboard' },
-				{ title: 'Companies', href: '/companies' },
-				{ title: 'Properties', href: '/properties' },
-				{ title: 'Users', href: '/users' },
-				{ title: 'Teams', href: '/teams' },
+				{ name: 'Dashboard', href: '/dashboard', current: true },
+				{ name: 'Companies', href: '/companies', current: false },
+				{ name: 'Properties', href: '/properties', current: false },
+				{ name: 'Users', href: '/users', current: false },
+				{ name: 'Teams', href: '/teams', current: false },
 			];
-
 	return (
-		<nav className="flex justify-between items-center bg-white shadow-md py-4 px-4 md:px-8">
-			<div className="text-xl md:text-2xl font-bold">
-				<Link to="/">
-					<img className="w-30 h-3xs" src="complitas_logo_with_text.png" />
-				</Link>
+		<Disclosure
+			as="nav"
+			className="relative bg-white dark:bg-gray-800/50 dark:after:pointer-events-none dark:after:absolute dark:after:inset-x-0 dark:after:bottom-0 dark:after:h-px dark:after:bg-white/10 drop-shadow-sm">
+			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+				<div className="relative flex h-16 items-center justify-between">
+					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+						{/* Mobile menu button*/}
+						<DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+							<span className="absolute -inset-0.5" />
+							<span className="sr-only">Open main menu</span>
+							<Bars3Icon
+								aria-hidden="true"
+								className="block size-6 group-data-open:hidden"
+							/>
+							<XMarkIcon
+								aria-hidden="true"
+								className="hidden size-6 group-data-open:block"
+							/>
+						</DisclosureButton>
+					</div>
+					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+						<div className="flex shrink-0 items-center">
+							<img
+								alt="Complitas"
+								src="complitas_logo_with_text.png"
+								className="h-8 w-auto"
+							/>
+						</div>
+						<div className="hidden sm:ml-6 sm:block">
+							<div className="flex space-x-4">
+								{navigation.map((item) => {
+									const isCurrent = location.pathname === item.href;
+
+									return (
+										<a
+											key={item.name}
+											href={item.href}
+											aria-current={isCurrent ? 'page' : undefined}
+											className={classNames(
+												isCurrent ?
+													'bg-gray-100 text-white dark:bg-gray-950/50'
+												:	'text-gray-300 hover:bg-white/5 hover:text-white',
+												'rounded-md px-3 py-2 text-sm font-medium'
+											)}>
+											{item.name}
+										</a>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+					{isAuthenticated ?
+						<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+							<Menu as="div" className="relative ml-3">
+								<MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+									<span className="absolute -inset-1.5" />
+									<span className="sr-only">Open user menu</span>
+									<img
+										alt=""
+										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+										className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+									/>
+								</MenuButton>
+
+								<MenuItems
+									transition
+									className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+									<MenuItem>
+										<a
+											href="#"
+											className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5">
+											Your profile
+										</a>
+									</MenuItem>
+									<MenuItem>
+										<a
+											href="#"
+											className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5">
+											Settings
+										</a>
+									</MenuItem>
+									<MenuItem>
+										<Button
+											label="Log out"
+											onClick={() =>
+												logout({
+													logoutParams: { returnTo: window.location.origin },
+												})
+											}
+											className="text-base p-2 md:py-2 md:px-4 text-[#646cff] disabled:hover"
+										/>
+									</MenuItem>
+								</MenuItems>
+							</Menu>
+						</div>
+					:	<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+							<Button
+								label="Log in"
+								onClick={() => loginWithRedirect()}
+								className="text-base p-2 md:py-2 md:px-4 text-green disabled:hover"
+							/>{' '}
+						</div>
+					}
+				</div>
 			</div>
-			<div className="flex gap-4 md:gap-8">
-				{navLinks.map((link) => (
-					<Link
-						to={link.href}
-						key={link.title}
-						className="text-base p-2 md:py-2 md:px-4 rounded transition-colors duration-200 hover:bg-white/10">
-						{link.title}
-					</Link>
-				))}
-				{!isAuthenticated ?
-					<Button
-						label="Log in"
-						onClick={() => loginWithRedirect()}
-						className="text-base p-2 md:py-2 md:px-4 text-[#646cff] disabled:hover"
-					/>
-				:	<Button
-						label="Log out"
-						onClick={() =>
-							logout({ logoutParams: { returnTo: window.location.origin } })
-						}
-						className="text-base p-2 md:py-2 md:px-4 text-[#646cff] disabled:hover"
-					/>
-				}
-			</div>
-		</nav>
+
+			<DisclosurePanel className="sm:hidden">
+				<div className="space-y-1 px-2 pt-2 pb-3">
+					{navigation.map((item) => (
+						<DisclosureButton
+							key={item.name}
+							as="a"
+							href={item.href}
+							aria-current={item.current ? 'page' : undefined}
+							className={classNames(
+								item.current ?
+									'bg-gray-100 text-white dark:bg-gray-950/50'
+								:	'text-gray-300 hover:bg-white/5 hover:text-white',
+								'block rounded-md px-3 py-2 text-base font-medium'
+							)}>
+							{item.name}
+						</DisclosureButton>
+					))}
+				</div>
+			</DisclosurePanel>
+		</Disclosure>
 	);
 }
