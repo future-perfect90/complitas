15f2fbc430ade6409e806ce3df587d6db4cce354
Luke Johnson <lukejohnson32@gmail.com>
2025-09-17 20:10:38 +0100

    send Email functionality

--- php/api/communications/sendEmail.php
+++ php/api/communications/sendEmail.php
@@ -2,9 +2,24 @@
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../../classes/Communication.php';
+require_once __DIR__ . '/../../classes/Auth.php';
 
-$communication = new Communication();
-$emailSent = $communication->sendEmail(['luke@complitas.co.uk'], 'test subject', 'test body123');
+//GET THE CONTENTS OF THE TEMPLATE
+$template = file_get_contents(__DIR__ . '/../../../templates/emailTemplate.html');
 
-http_response_code(200);
-echo json_encode(['success' => true, 'message' => $emailSent]);
+$data = json_decode(file_get_contents("php://input"), true);
+
+if ($data && isset($data['to'], $data['subject'], $data['body'])) {
+    //REPLACE THE CONTENT OF THE EMAIL WITH THE VARIABLES
+    $body = str_replace('{{user_name}}', $data['to'], $template);
+    $body = str_replace('{{certifications}}', $data['body'], $body);
+
+    $communication = new Communication();
+    $emailSent = $communication->sendEmail($data['to'], $data['subject'], $body);
+
+    http_response_code(200);
+    echo json_encode(['success' => true, 'message' => $emailSent]);
+} else {
+    http_response_code(400);
+    echo json_encode(['success' => false, 'message' => 'Invalid input. Please provide to, subject, and body.']);
+}
