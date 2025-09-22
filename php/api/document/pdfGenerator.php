82abfdfc74e02a25cca2354d97bc9e0a9dcf9ac9
Luke Johnson <lukejohnson32@gmail.com>
2025-09-22 20:45:42 +0100

    remove unused file

--- php/api/document/pdfGenerator.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../../shared/headers.php';
-require_once __DIR__ . '/../../../vendor/autoload.php';
-require_once __DIR__ . '/../../classes/Document.php';
-
-(new Document())->generatePDF();
