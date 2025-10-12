27f428a10834528ad7f15228f4de914c1fa5e158
Luke Johnson <lukejohnson32@gmail.com>
2025-10-12 16:00:35 +0100

    fix profile page

--- php/api/user-management/getProfile.php
+++ php/api/user-management/getProfile.php
@@ -15,19 +15,10 @@ $userId = $_GET['user_id'] ?? '';
 $user = new User($db);
 $profileData = $user->getUserProfile($userId);
 
-$teams = new Teams($db);
-$userTeams = $teams->getMyTeams($userId);
-
-//GET properties which belong to the user's teams
-$properties = new Properties($db);
-$userProperties = $properties->getPropertiesByTeamIds(array_column($userTeams, 'id'));
-
 $response = [
     'success' => true,
     'data' => [
         'profile' => $profileData,
-        'teams' => $userTeams,
-        'properties' => $userProperties
     ]
 ];
 echo json_encode($response);
